cf64f86b2d4a18def0d24f07087a4031e2f952f2
Leah N-C <Lniemcav@gmail.com>
2022-02-20 01:41:45 -0500

    The game is working, but the doors are all still unlocked.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function ask(questionText) {
 /* ----------------- Room Class Constructor ------------------ */
 
 class Room {
-  constructor(name, description, inventory, locked = true) {
+  constructor(name, description, inventory, locked) {
     this.name = name;
     this.description = description;
     this.inventory = inventory;
@@ -38,35 +38,35 @@ class Item {
 
 let bedroom = new Room(
   "bedroom",
-  "You are in the bedroom. \nThere is a map on the bedside table. ",
+  "\nYou are in the bedroom. \nThere is a map on the bedside table. ",
   ["map", "key"],
   false
 );
 
 let bathroom = new Room(
   "bathroom",
-  "You are in the bathroom. \nOn the mirror is a sticker. ",
+  "\nYou walk into the bathroom. \nOn the mirror is a sticker. ",
   ["sticker"],
   true
 );
 
 let sunroom = new Room(
   "sunroom",
-  "You are in the living room. \nThere is a book on the table. ",
+  "\nYou are now in the sunroom. \nThere is a book on the table. ",
   ["book"],
   false
 );
 
 let kitchen = new Room(
   "kitchen",
-  "You are in the kitchen. \nThere is a microwave in here. ",
+  "\nYou are in the kitchen. \nThere is a microwave in here. ",
   ["microwave"],
   false
 );
 
 let porch = new Room(
   "porch",
-  "You are on the porch. \nThere is a chair. ",
+  "\nYou go onto the porch. \nThere is a chair. ",
   ["chair", "keypad"],
   false
 );
@@ -75,50 +75,50 @@ let outside = new Room(
   "outside",
   "\nYou escaped!!! Great job 🥳 💃🏻 \n\nYou may now move between rooms freely or end the game. ",
   [],
-  locked = true
+  true
 );
 
 /* -------------------- Item Descriptions --------------------- */
 
 let map = new Item(
   "map",
-  "You pick up the map and start reading. There is a note at the top. \nIt reads: 'You are locked in this room and must find the key to get out. \nOnce you leave this room, you will need to find the exit and leave the house to win the game. \nAt any point, you may type 'quit' or 'q' to quit the game. \nYou may type 'inventory' or 'i' to view your inventory. \nYou may use words like 'take' and 'examine' to interact with items in the house.' \nYou see on the map that you are in the bedroom and there are 4 more rooms in the house: the bathroom, the living room, the kitchen and the porch. \nAs you look around, you notice a key in the corner. ",
+  "\nYou pick up the map and start reading. There is a note at the top. \nIt reads: 'You are locked in this room and must find the key to get out. \nOnce you leave this room, you will need to find the exit and leave the house to win the game. \nAt any point, you may type 'quit' or 'q' to quit the game. \nYou may type 'inventory' or 'i' to view your inventory. \nYou may use words like 'take' and 'examine' to interact with items in the house. \nLook closely at the map, this part is important (you may want to take notes...)' \nYou see on the map that you are in the bedroom and there are 4 more rooms in the house: the bathroom, the living room, the kitchen and the porch. \nThey are connected in that order. The porch leads to outside. \n\nAs you look around, you notice a key in the corner. ",
   "bedroom",
   false
 );
 let key = new Item(
   "key",
-  "You pick up the key. It looks like it fits in the locked door. ",
+  "\nYou pick up the key. It looks like it fits in the locked door. ",
   "bedroom",
   true
 );
 let sticker = new Item(
   "sticky map",
-  "You pick up the sticker. All that is written on it is the number '3'. ",
+  "\nYou pick up the sticker. All that is written on it is the number '3'. ",
   "bathroom",
   false
 );
 let book = new Item(
   "book",
-  "Inside of the book, someone has written the number '6'. ",
+  "\nInside of the book, someone has written the number '6'. ",
   "sunroom",
   false
 );
 let microwave = new Item(
   "microwave",
-  "You notice that on the door of the microwave, there is a '7' written. ",
+  "\nYou notice that on the door of the microwave, there is a '7' written. ",
   "kitchen",
   false
 );
 let chair = new Item(
   "chair",
-  "When you look more closely, you see that there is a mumber on the left arm of the chair. The number is '4'. \nYou look around and see a keypad on the door to outside. ",
+  "\nWhen you look more closely, you see that there is a mumber on the left arm of the chair. The number is '4'. \nYou look around and see a keypad on the door to outside. ",
   "porch",
   false
 );
 let keypad = new Item(
   "keypad",
-  "Which numbers would you like to enter? ",
+  "\nWhich numbers would you like to enter? ",
   "porch",
   false
 );
@@ -181,26 +181,23 @@ function sanitize(userInput) {
 function changeRoom(newLocation) {
   //Look at the roomStateMachine for current location
   let allowedTransitions = roomStateMachine[currentLocation];
-  console.log(allowedTransitions)
-  //If the new location's door is locked
-  if (player.inventory.includes("key")) {
-    bathroom.locked = false;
+  //If requested room is connected to current room
+  if (allowedTransitions.includes(newLocation)) {
+    //The requested room becomes the current room (change to that room)
     currentLocation = newLocation;
+    //Print the description of the new room
     console.log(roomLookup[currentLocation].description);
-  } else if (newLocation.locked = true) {
-    console.log("You need to unlock the door first. ");
   }
-  //If current location is connected to the new location
-  else if (allowedTransitions.includes(newLocation)) {
-    //Then current location becomes the new location
-    currentLocation = newLocation;
-    //And computer prints the new room's description
-    console.log(roomLookup[currentLocation].description);
+  //If the requested location is locked
+  else if (newLocation.locked === true) {
+    //Print this message
+    console.log("\nYou need to unlock the door first");
   }
   //If currentLocation and newLocation are not connected
   else {
+    //Print this
     console.log(
-      "You cannot move directly from " +
+      "\nYou cannot move directly from " +
         currentLocation +
         " to " +
         newLocation +
@@ -214,46 +211,66 @@ function changeRoom(newLocation) {
 /* ------------------ Function to Take Items ------------------ */
 
 function takeItem(suggestedItem) {
+  //If the current location has the requested item and the item is takeable
   if (
     roomLookup[currentLocation].inventory.includes(suggestedItem) &&
     itemLookup[suggestedItem].takeable === true
   ) {
-    console.log("Taken");
+    //Print this and add to player's inventory
+    console.log("\nTaken");
     player.inventory.push(suggestedItem);
-  } else if (
+  }
+  //If the room has the item but the item is not takeable
+  else if (
     roomLookup[currentLocation].inventory.includes(suggestedItem) &&
-    itemLookup[suggestedItem].takeable === false
+    !itemLookup[suggestedItem].takeable
   ) {
-    console.log("You cannot take that! ");
-  } else {
-    console.log("I don't see a " + suggestedItem + ". ");
+    //Print this
+    console.log("\nYou cannot take that! ");
+  }
+  //Otherwise, if the item is not in the room
+  else {
+    //Inform user can't see the item
+    console.log("\nI don't see a " + suggestedItem + ". ");
   }
 }
 
 /* ------------------ Function to Drop Items ------------------ */
 
 function dropItem(inventoryItem) {
-  //If the player has an item in inventory
-  if (player.inventory.includes(inventoryItem) && roomLookup[currentLocation].inventory.includes(inventoryItem)) {
+  //If player has an item in inventory and the item is from the current room
+  if (
+    player.inventory.includes(inventoryItem) &&
+    roomLookup[currentLocation].inventory.includes(inventoryItem)
+  ) {
     //Drop the item into the current room
-    let droppedItem = player.inventory.splice(player.inventory.indexOf(inventoryItem));
+    let droppedItem = player.inventory.splice(
+      player.inventory.indexOf(inventoryItem)
+    );
     droppedItem.push(inventoryItem);
-    console.log("You dropped the " + inventoryItem + " from your inventory. ");
+    //And inform user that it has dropped
+    console.log("\nYou dropped the " + inventoryItem + " from your inventory. ");
   } //If the item doesn't belong in the current room, tell player to drop in the right room
   else {
-    console.log("Please put that back where you found it. ");
+    console.log("\nPlease put that back where you found it. ");
   }
 }
 
 /* -------------------------- Keypad Puzzle ----------------------- */
 
 function keypadPuzzle(numbers) {
+  //If user enters the correct code
   if (numbers === "3674") {
-    outside.locked = false;
+    //The door becomes unlocked
+    outside.locked === false;
+    //And player is outside
+    changeRoom(outside);
     console.log(outside.description);
-  } else {
-    outside.locked = true;
-    console.log("The password is incorrect. Please type 'a' to try again. ");
+  }
+  //Otherwise, the door remains locked and user needs to try code again
+  else {
+    outside.locked === true;
+    console.log("\nThe password is incorrect. Please type 'a' to try again. ");
   }
 }
 
@@ -275,8 +292,9 @@ async function start() {
 async function playGame() {
   //While shit and stuff is truthy (meaning the user has not typed "quit")
   while (true) {
+    console.log("\n-- current location: " + currentLocation + " --");
     //Ask this question after every response
-    let answer = await ask("\nWhat would you like to do? \n>_");
+    let answer = await ask("What would you like to do? \n>_");
     //Use sanitize function (above) to clean user's answer
     answer = sanitize(answer);
     //Split answer by spaces into an array of words
@@ -284,28 +302,37 @@ async function playGame() {
     //Find the last word in the user's sentence
     let lastWordInAnswer =
       splitAnswerIntoArray[splitAnswerIntoArray.length - 1];
-    //If the user's input includes "read" and "map" and current location is bedroom, read the map's description
+    //If the user's input includes "read" or "examine" and "map" and current location is bedroom, read the map's description
     if (
-      (answer.includes("read") || answer.includes("examine")) &&
+      (answer.includes("read") || answer.includes("look at") || answer.includes("examine")) &&
       answer.includes("map") &&
       currentLocation === "bedroom"
     ) {
+      //Print map's description
       console.log(map.description);
+    } 
+    //If input includes "open" or "unlock" and current location is bedroom, run "bathroom" through changeRoom() function
+    else if (
+      (answer.includes("open") || answer.includes("unlock")) &&
+      currentLocation === "bedroom"
+    ) {
+      console.log(currentLocation)
+      changeRoom("bathroom");
     }
-    //If the user's input includes "take", run the item through takeItem() function
+    //If the user's input includes "take" or "pick up", run the item through takeItem() function
     else if (answer.includes("take") || answer.includes("pick up")) {
       takeItem(lastWordInAnswer);
-    } else if (answer.includes("inventory") || answer === "i") {
+    }
+    //If the input includes "inventory" or "i", inform user of whats's in inventory
+    else if (answer.includes("inventory") || answer === "i") {
       console.log("Your inventory includes: " + "[ " + player.inventory + " ]");
-    } else if(answer.includes("drop") || answer.includes("put down")) {
+    }
+    //If the input includes "drop" or "put down", run item through dropItem() function
+    else if (answer.includes("drop") || answer.includes("put down")) {
       dropItem(lastWordInAnswer);
-    } 
-    else if (answer.includes("open") ||
-        answer.includes("unlock") ||
-        answer.includes("bathroom")
-    ) {
-      changeRoom("bathroom");
-    } else if (
+    }
+    //If input includes any of these words
+    else if (
       answer.includes("bedroom") ||
       answer.includes("bathroom") ||
       answer.includes("sunroom") ||
@@ -313,8 +340,11 @@ async function playGame() {
       answer.includes("porch") ||
       answer.includes("outside")
     ) {
+      //Run the word through changeRoom() function
       changeRoom(lastWordInAnswer);
-    } else if (
+    }
+    //If input includes "examine" or "look at" and any of the item words below, print the item's description
+    else if (
       (answer.includes("examine") || answer.includes("look at")) &&
       answer.includes("sticker")
     ) {
@@ -334,45 +364,25 @@ async function playGame() {
       answer.includes("chair")
     ) {
       console.log(chair.description);
-    } else if (
-      (answer.includes("examine") || answer.includes("look at")) &&
-      answer.includes("keypad") || answer === "a"
+    }
+    //If input includes "examine" or "look at" and "keypad", or if input is "a" and current location is "porch", await ask keypad's description and run answer through keypadPuzzle() function
+    else if (
+      (((answer.includes("examine") || answer.includes("look at")) &&
+        answer.includes("keypad")) ||
+        answer === "a") &&
+      currentLocation === "porch"
     ) {
       answer = await ask(keypad.description);
       keypadPuzzle(answer);
-    } else if (answer === "quit" || answer === "q") {
+    }
+    //If user types "quit" or "q", end the game
+    else if (answer === "quit" || answer === "q") {
       console.log("Goodbye");
       process.exit();
-    } else {
+    }
+    //Otherwise, print this
+    else {
       console.log("I do not know how to " + answer + ". ");
     }
   }
 }
-
-//Function to play the game
-
-//Print explaination of game details
-// Can quit at any time
-// Can use action words : take, use,
-
-//While loop for as long as the game is played
-
-//if the user's response includes "map" and "read" and they are in the bedroom, read the map
-
-//else if the user's response includes "take" and "key" and they are in the bedroom, print "taken" and add key to player's inventory
-
-//else if the user's response includes "open" or "unlock" and "door" AND player's location is the bedroom, change location to bathroom and print bathroom description
-
-//else if user's response includes "take" and "map" and player's location is bathroom, print "taken" and add sticky map to inventory
-
-//
-
-//else if the user's response
-
-//
-
-//else if user's response includes "take", print "you cannot take that"
-
-//else if the user types "quit", quit the game
-
-//else "I don't know how to..."
